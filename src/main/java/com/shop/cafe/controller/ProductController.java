7f05a192a78984b19fa0a7c06ad48d29f6d17812
Lacheln <psionict@naver.com>
2025-03-07 14:43:35 +0900

    feat:insertMember완료

--- src/main/java/com/shop/cafe/controller/ProductController.java
+++ src/main/java/com/shop/cafe/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import com.shop.cafe.dto.Product;
 import com.shop.cafe.service.ProductService;
 
 @RestController
+@CrossOrigin("http://127.0.0.1:5500/")
 public class ProductController {
 	
 	@Autowired
